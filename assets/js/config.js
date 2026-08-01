9cfb4f613aa30b63c473df296901f691eacdeda3
yoseff33 <143504036+yoseff33@users.noreply.github.com>
2026-08-01 09:31:04 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = Object.freeze({
-  supabaseUrl: "PUT_YOUR_SUPABASE_URL_HERE",
-  supabaseAnonKey: "PUT_YOUR_SUPABASE_ANON_KEY_HERE",
+  supabaseUrl: "sb_publishable_L8GNpYh4zsoQ_KG44Cc-WQ_M-X6Wnuj",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InppY3Jtb2FhYWJ4dmtwenV1aXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1NjI1MDgsImV4cCI6MjEwMTEzODUwOH0.od9WR8hz4o57RZvAorM7BGoeh7i7SwURdetzJ1cesKU",
   business: {
     name: "مؤسسة يوسف عيد المطيري لقطع غيار السيارات",
     commercialRegistration: "7054534024",
